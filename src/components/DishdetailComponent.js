29b20a39c23c60c93af91fc56b0fd6c126524a75
Aman Agrawal <amanagrawal2098@gmail.com>
2020-05-10 23:23:26 +0530

    animations are added

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -95,21 +96,25 @@ class CommentForm extends Component{
     
         if(comment!=null)
         {
+            
         const cmts=comment.map((comment)=>{
             return(
+                <Fade in>
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                     </p>
                    </li>
-                
+                </Fade>
         )});
         return (
             <div>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
+                <Stagger in>
                     {cmts}  
+                    </Stagger>
                     </ul>  
                     <CommentForm dishId={dishId} postComment={postComment} />  
             </div>
@@ -122,6 +127,11 @@ function   RenderDish({dish}) {
        
             return(
               <div className='col-12 col-md-5 m-1'>
+                  <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
                 <Card>
                     <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
@@ -129,9 +139,7 @@ function   RenderDish({dish}) {
                       <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-                
-                
-                
+                </FadeTransform>
             </div>
             );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import Contact from './ContactComponent';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { postComment, fetchDishes, fetchComments, fetchPromos} from '../redux/ActionCreators';
 const mapStateToProps = (state) => {
   return {
@@ -71,7 +72,9 @@ componentDidMount() {
       <div>
         <Header />
         <div>
-          <Switch>
+        <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch location={this.props.location}>
               <Route path='/home' component={HomePage} />
               <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
@@ -79,6 +82,8 @@ componentDidMount() {
               <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
           </Switch>
+          </CSSTransition>
+          </TransitionGroup>
         </div>
         <Footer />
       </div>
