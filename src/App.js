c811fcde553e96bae8d9a89b0efba5cecadcaecb
Aman Agrawal <amanagrawal2098@gmail.com>
2020-05-11 23:03:29 +0530

    app.css warning removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import Main from './components/MainComponent';
-import css from './App.css';
+import  './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
